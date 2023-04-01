c8cede71a1aca07ea3f659be7542c9b67a0c40de
Dario M <dario.aviles@gmail.com>
2023-04-01 16:29:52 +0200

    Zip content before save

--- downloader.py
+++ downloader.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python3
+
 import concurrent.futures
 import json
 import logging
 import os
 import time
+from typing import Any
+from zipfile import ZIP_DEFLATED, ZipFile
 
 import requests
+from fake_headers import Headers
 
-ALI_URL = f'https://www.alimerkaonline.es/ali-ws/'
+ALI_URL = 'https://www.alimerkaonline.es/ali-ws/'
 ALI_TOKEN = ALI_URL + 'acceso/cp/{cp}'
 SHOP_URL = ALI_URL + 'tienda/direcciones'
 LOCKER_URL = ALI_URL + 'tienda/direcciones/lockers'
@@ -20,89 +25,78 @@ logger = logging.getLogger(__name__)
 logger.addHandler(ch)
 logger.setLevel(logging.DEBUG)
 
-WAIT = 0.5
+WAIT = .5
 
 
 def jsonp2json(text: str):
     return text[3:-1]
 
 
-def worker(sf: dict[str], postal_code: str, point_of_service: str, **kwargs) -> None:
+def worker(sf: dict[str, Any], postal_code: str, point_of_service: str, **kwargs) -> tuple[str, dict]:
     logger.info(f'Download subfamily "{sf["nombre"]}" from "{point_of_service}"')
+
     res = requests.get(PRODUCTS_URL.format(subfamily=sf['codsubfamilia'], cp=postal_code),
-                       params=kwargs)  # GET
+                       params=kwargs, headers=Headers().generate())  # GET
 
     res.raise_for_status()
     res = json.loads(jsonp2json(res.text))
-    with open(os.path.join(d2, str(sf['codsubfamilia'])) + '.json', 'w',
-              encoding='utf8') as g:
-        json.dump(res, g, indent=4, ensure_ascii=False)
     time.sleep(WAIT)
+    return str(sf['codsubfamilia']) + '.json', res
 
 
 if __name__ == '__main__':
     # Get timestamp
     timestamp = time.time_ns()  # nanoseconds
     cps = [33404]
-    for cp in cps:
-        # Get auth token
-        r = requests.post(ALI_TOKEN.format(cp=cp))
-        r.raise_for_status()
-        r = r.json()
-        iauthtoken = r['iauthtoken']
-        params = {'iauthtoken': iauthtoken}
+    # https://superfastpython.com/multithreaded-zip-files/#Zip_Files_Concurrently_With_Threads_Without_a_Lock
+    base_path = 'data'
+    loc_dict = {'lockers': LOCKER_URL, 'stores': SHOP_URL}
+    with ZipFile(os.path.join(base_path, f'{timestamp}.zip'), 'w', compression=ZIP_DEFLATED) as z:
 
-        # Point of service ids
-        poss = set()
-
-        # Get lockers
-        r = requests.get(LOCKER_URL, params=params)
-        r.raise_for_status()
-        r = json.loads(jsonp2json(r.text))
-        d = f'data/{timestamp}/{cp}/lockers/'
-        os.makedirs(d)
-        for section in r:
-            poss.add(str(section['id']))
-            with open(os.path.join(d, str(section['id'])) + '.json', 'w', encoding='utf8') as f:
-                json.dump(section, f, indent=4, ensure_ascii=False)
-
-        # Get stores
-        r = requests.get(SHOP_URL, params=params)
-        r.raise_for_status()
-        r = json.loads(jsonp2json(r.text))
-        d = f'data/{timestamp}/{cp}/stores/'
-        os.makedirs(d)
-        for section in r:
-            poss.add(str(section['id']))
-            with open(os.path.join(d, str(section['id'])) + '.json', 'w', encoding='utf8') as f:
-                json.dump(section, f, indent=4, ensure_ascii=False)
-
-        # Get the rest
-        for pos in poss:
-            r = requests.get(LISTING_URL.format(pos=pos, cp=cp), params=params)  # GET
+        for cp in cps:
+            # Get auth token
+            r = requests.post(ALI_TOKEN.format(cp=cp), headers=Headers().generate())
             r.raise_for_status()
-            r = json.loads(jsonp2json(r.text))
+            r = r.json()
+            iauthtoken = r['iauthtoken']
+            params = {'iauthtoken': iauthtoken}
 
-            d1 = f'data/{timestamp}/{cp}/sections/{pos}'
-            os.makedirs(d1)
-            d2 = f'data/{timestamp}/{cp}/subfamilies/{pos}'
-            os.makedirs(d2)
+            # Point of service ids
+            poss = set()
 
-            for section in r:  # Section
-                with open(os.path.join(d1, str(section['codseccion'])) + '.json', 'w', encoding='utf8') as f:
-                    json.dump(section, f, indent=4, ensure_ascii=False)
+            for place, url in loc_dict.items():
 
-                # for family in section['familias']:
+                # Get lockers
+                r = requests.get(url, params=params, headers=Headers().generate())
+                r.raise_for_status()
+                r = json.loads(jsonp2json(r.text))
 
-                # We can use a with statement to ensure threads are cleaned up promptly
-                with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-                    future_to_subfamily = {executor.submit(worker, subfamily, cp, pos, **params): subfamily
-                                           for family in section['familias'] for subfamily in
-                                           family['subfamilias']}
+                for section in r:
+                    poss.add(str(section['id']))
+                    z.writestr(os.path.join(f'{cp}/{place}/', str(section['id'])) + '.json',
+                               json.dumps(section, ensure_ascii=False))
 
-                    for future in concurrent.futures.as_completed(future_to_subfamily):
-                        url = future_to_subfamily[future]
-                        try:
-                            future.result()
-                        except Exception as exc:
-                            raise exc
+            # Get the rest
+            for pos in poss:
+                r = requests.get(LISTING_URL.format(pos=pos, cp=cp), params=params, headers=Headers().generate())  # GET
+                r.raise_for_status()
+                r = json.loads(jsonp2json(r.text))
+
+                for section in r:  # Section
+                    z.writestr(f'{cp}/sections/{pos}/{str(section["codseccion"]) + ".json"}',
+                               json.dumps(section, ensure_ascii=False))
+
+                    # We can use a with statement to ensure threads are cleaned up promptly
+                    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                        future_to_subfamily = {executor.submit(worker, subfamily, cp, pos, **params): subfamily
+                                               for family in section['familias'] for subfamily in
+                                               family['subfamilias']}
+
+                        for future in concurrent.futures.as_completed(future_to_subfamily):
+                            url = future_to_subfamily[future]
+                            try:
+                                p, j = future.result()
+                                z.writestr(os.path.join(f'{cp}/subfamilies/{pos}', p),
+                                           json.dumps(j, ensure_ascii=False))
+                            except Exception as exc:
+                                raise exc
